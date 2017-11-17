195b8e4833ae2def33320fc29aefaa15bff1a863
Andy Russell <arussell123@gmail.com>
2017-11-17 12:34:15 -0500

    pull coverage from github instead of firebase

--- static/main.js
+++ static/main.js
@@ -1,38 +1,54 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var tasksTemplate = Handlebars.compile(document.getElementById('tasks-template').textContent);
-  var firebase = new Firebase("https://rosetta-coverage.firebaseio.com/");
-  firebase.child("tasks").once("value", function(snapshot) {
-    var html = tasksTemplate(snapshot.val());
-    document.getElementById('tasks').innerHTML = html;
+function appendTasks(tasks) {
+  var tasksTemplate = Handlebars.compile(
+      document.getElementById('tasks-template').textContent);
 
-    var taskList = new List('tasks', {
-      valueNames: [
-        'name',
-        { 'name': 'title', 'attr': 'data-title' },
-        { 'name': 'local', 'attr': 'title' },
-        { 'name': 'remote', 'attr': 'title' },
-      ]
-    });
+  var html = tasksTemplate(tasks);
+  document.getElementById('tasks').innerHTML = html;
 
-    var radioButtons = document.querySelectorAll('input[type=radio');
-
-    [].forEach.call(radioButtons, function(element) {
-      element.addEventListener('click', function(event) {
-        taskList.filter();
-
-        var filter = (event.srcElement || event.target).value;
-        taskList.filter(function(item) {
-          if (filter === 'local') {
-            return item.values().local === 'Yes' && item.values().remote === 'No';
-          } else if (filter === 'remote') {
-            return item.values().remote === 'Yes' && item.values().local === 'No';
-          } else if (filter === 'unimplemented') {
-            return item.values().remote === 'No' && item.values().local === 'No';
-          }
-
-          return true;
-        });
-      })
-    });
+  var taskList = new List('tasks', {
+    valueNames: [
+      'name',
+      { 'name': 'title', 'attr': 'data-title' },
+      { 'name': 'local', 'attr': 'title' },
+      { 'name': 'remote', 'attr': 'title' },
+    ]
   });
+
+  var radioButtons = document.querySelectorAll('input[type=radio');
+
+  [].forEach.call(radioButtons, function(element) {
+    element.addEventListener('click', function(event) {
+      taskList.filter();
+
+      var filter = (event.srcElement || event.target).value;
+      taskList.filter(function(item) {
+        if (filter === 'local') {
+          return item.values().local === 'Yes' && item.values().remote === 'No';
+        } else if (filter === 'remote') {
+          return item.values().remote === 'Yes' && item.values().local === 'No';
+        } else if (filter === 'unimplemented') {
+          return item.values().remote === 'No' && item.values().local === 'No';
+        }
+
+        return true;
+      });
+    })
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET",
+    "https://raw.githubusercontent.com/Hoverbear/rust-rosetta/gh-pages/coverage.json",
+    true);
+  xhr.onload = function(e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        appendTasks(JSON.parse(xhr.responseText));
+      } else {
+        console.error(xhr.statusText);
+      }
+    }
+  }
+  xhr.send(null);
 });
